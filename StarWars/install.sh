879bb99bd470c575bf0ca13969696d01f1ff9735
DarkXero <81101439+DarkXero-dev@users.noreply.github.com>
2024-12-13 17:25:13 +0200

    Update install.sh

--- StarWars/install.sh
+++ StarWars/install.sh
@@ -83,6 +83,7 @@ if [ "$UID" -eq "$ROOT_UID" ]; then
 
   # Success message
   prompt -s "\n\t          ***************\n\t          *  installed!  *\n\t          ***************\n"
+  exit 1
 
 else
 
